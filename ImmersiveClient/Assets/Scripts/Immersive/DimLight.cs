7aa02aaf870f966bc6d959fa053d14d45f5ac44c
karsuszhang <yifanzhang@10.0.0.5>
2016-07-23 16:27:35 +0800

    map node try, leaving node dim light

---
+++ ImmersiveClient/Assets/Scripts/Immersive/DimLight.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DimLight : MonoBehaviour {
+
+    private Light m_Light;
+    private float m_OrgIntensity = 0f;
+  
+    const float BaseIntensity = 0.2f;
+    const float DimTime = 3f;
+
+    private bool IsDim = false;
+    private float m_TimeCount = 0f;
+	// Use this for initialization
+	void Start () {
+        m_Light = GetComponent<Light>();
+        m_OrgIntensity = m_Light.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsDim)
+        {
+            m_TimeCount += Time.deltaTime;
+            float ratio = Mathf.Min(1f, m_TimeCount / DimTime);
+
+            m_Light.intensity = BaseIntensity + (1f - ratio) * (m_OrgIntensity - BaseIntensity);
+            if (m_TimeCount >= DimTime)
+                IsDim = false;
+        }
+	}
+
+    public void Dim()
+    {
+        IsDim = true;
+    }
+}
