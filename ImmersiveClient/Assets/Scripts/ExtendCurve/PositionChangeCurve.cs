f064d4912cc88e7f3ee531b034dd541c61badf8c
karsuszhang <yifanzhang@10.0.0.15>
2016-08-13 00:27:42 +0800

    new node type test, new curve

---
+++ ImmersiveClient/Assets/Scripts/ExtendCurve/PositionChangeCurve.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionChangeCurve : AnimatedCurve {
+
+    public Vector3 From = Vector3.zero;
+    public Vector3 Change = Vector3.zero;
+
+    protected override void OnUpdate(float factor, bool isFinished)
+    {
+        this.transform.localPosition = From + factor * Change;
+    }
+}
